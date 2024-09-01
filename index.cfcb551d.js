b2b37f0b5e6195e416583c54a2cf86a99fff2c65
Serhii Babskyi <sergbald99@gmail.com>
2024-09-01 17:14:41 +0300

    make build

---
+++ index.cfcb551d.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),c=t.clientWidth,o=t.clientHeight,l=e.offsetWidth,n=e.offsetHeight,i=function(t,e){return(t-e)/2},r=i(c,l),f=i(o,n);e.style.left="".concat(r,"px"),e.style.top="".concat(f,"px");
+//# sourceMappingURL=index.cfcb551d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const fild = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const fildWidth = fild.clientWidth;
+const fildHeight = fild.clientHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+const getCenterCoordinates = (widthParent, widthChild)=>{
+    return (widthParent - widthChild) / 2;
+};
+const centerByX = getCenterCoordinates(fildWidth, spiderWidth);
+const centerByY = getCenterCoordinates(fildHeight, spiderHeight);
+spider.style.left = `${centerByX}px`;
+spider.style.top = `${centerByY}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
